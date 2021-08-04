42b6cac4c8ef4eec7ce96ca2462366a9c0d3e999
Alasdair Nottingham <alasdair.nottingham@gmail.com>
2021-08-04 11:26:49 -0400

    Chapter 7 source

---
+++ Chapter07/src/main/java/com/demo/FileSystemConfigSource.java
@@ -0,0 +1,42 @@
+package com.demo;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class FileSystemConfigSource implements ConfigSource {
+    private File dir = new File("secrets");
+
+    @Override
+    public Set<String> getPropertyNames() {
+        Set<String> props = Arrays.asList(dir.listFiles()).stream().map(f -> f.getName()).collect(Collectors.toSet());
+
+        return props;
+    }
+
+    @Override
+    public String getValue(String s) {
+        File f = new File(dir, s);
+        try {
+            return new String(Files.readAllBytes(new File(dir, s).toPath()), StandardCharsets.UTF_8);
+        } catch (IOException ioe) {
+            return null;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "kube.secret";
+    }
+
+    @Override
+    public int getOrdinal() {
+        return 5;
+    }
+}
